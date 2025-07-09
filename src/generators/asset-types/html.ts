8234f4fb163ffd61df2361712dfaa454cb80e14d
Maiko Tan <maiko.tan.coding@gmail.com>
2025-07-09 11:17:09 +0800

    fix: ensure default tag is 'i' when not provided in HTML template rendering

--- src/generators/asset-types/html.ts
+++ src/generators/asset-types/html.ts
@@ -3,7 +3,10 @@ import { renderTemplate } from '../../utils/template';
 
 const generator: FontGenerator = {
   generate: async options => {
-    return renderTemplate(options.templates.html, options);
+    return renderTemplate(options.templates.html, {
+      ...options,
+      tag: options.tag === '' ? 'i' : options.tag
+    });
   }
 };
 
